dc4827cfca86017f70623b998c13e47d646030ef
Trai Nguyễn Chí <traitai73@gmail.com>
2025-06-23 02:43:50 +0700

    fix bug server error

--- app.py
+++ app.py
@@ -381,7 +381,7 @@ async def ping():
         analyzer = RealtimeCryptoPatternAnalyzer()
         
         # Chạy phân tích và lưu dữ liệu
-        analyzer.run_analysis(symbol='BTC/USDT', timeframe='1h', order_realtime=1, order_reliable=20, threshold=0.02, lookback_window=10)
+        analyzer.run_analysis(symbol='BTC/USDT', timeframe='1h', order_realtime=1, order_reliable=20, threshold=0.01, lookback_window=10)
         analyzer.run_analysis(symbol='ETH/USDT', timeframe='1h', order_realtime=1, order_reliable=20, threshold=0.02, lookback_window=10)
         analyzer.run_analysis(symbol='SOL/USDT', timeframe='1h', order_realtime=1, order_reliable=20, threshold=0.025, lookback_window=10)
         analyzer.run_analysis(symbol='XRP/USDT', timeframe='1h', order_realtime=1, order_reliable=20, threshold=0.015, lookback_window=10)
